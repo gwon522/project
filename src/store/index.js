a2d1c5ed9849d35242e7269b85dced033598b066
gwon <53310111+gwon522@users.noreply.github.com>
2021-11-21 20:47:05 +0900

    project start

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { createStore, applyMiddleware } from 'redux';
+import { cretaeSagaMiddleware } from 'redux-saga'
+import { logger } from 'react-logger'
+import { composeWithDevTools } from 'react-devtools'
+import rootReducer from './modules/index'
+import rootSaga from './sagas/index'
+
+export default function configureStore() {
+    const sagaMiddleware = cretaeSagaMiddleware();
+    const middlewares = [sagaMiddleware, logger];
+    const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)),);
+    sagaMiddleware.run(rootSaga);
+
+    return store;
+}
