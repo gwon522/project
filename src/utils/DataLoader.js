c5d196d60b1fb2a00d6c36b5f80eb62d826f4811
gwon522 <nesaz0522@naver.com>
2021-11-30 17:54:14 +0900

    [ADD] API 호출 공통

---
+++ src/utils/DataLoader.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const settingAxios = axios.create({
+    baseURL: `localHost:3000`,
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+    },
+});
+
+export const DataLoader = (method, uri, data) => {
+    switch (method) {
+        case 'get':
+            return settingAxios.get(uri);
+        case 'post':
+            return settingAxios
+                .post(uri, JSON.stringify(data))
+                .then((result) => result.data);
+        default:
+            throw Error('not exist Method');
+    }
+};
