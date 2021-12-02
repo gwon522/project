ff29301d8522370e57ab0745a94f9a4b38f58cd3
gwon522 <nesaz0522@naver.com>
2021-12-02 18:41:12 +0900

    [ADD, UPDATE] 글로벌 스타일 추가 수정 필요, styled-reset 추가

--- src/styles/Global.style.js
+++ src/styles/Global.style.js
@@ -1,33 +1,31 @@
 import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
+    ${reset}
+    a{
+        text-decoration:none;
+        color : inherit;
+    }
     *{
-        margin:0;
-        padding:0;
+        box-sizing: border-box;
     }
     body{
-        box-sizing: border-box;
+        font-size : 14px;
     }
 `;
 const size = {
-    mobile: '770px',
-    tabletS: '1023px',
-    tabletM: '1220px',
-    tabletL: '1280px',
-    laptop: '1460px',
-    desktop: '1700px',
+    mobile: '767px',
+    desktop: '1023px',
 };
 
 const theme = {
-    comColor: '#',
     comFontSize: '',
     backgroundColor: '',
     mobile: `(max-width:${size.mobile})`,
-    tabletS: `(max-width:${size.tabletS})`,
-    tabletM: `(max-width:${size.tabletM})`,
-    tabletL: `(max-width:${size.tabletL})`,
-    laptop: `(min-width:${size.laptop})`,
-    desktop: `(min-width:${size.desktop})`,
+    tablet: `(max-width:${size.desktop})`,
+    // desktop: `(min-width:${size.desktop})`,
 };
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default { GlobalStyle, theme };
