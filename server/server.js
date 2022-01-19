9cdc18d4770416318d53e7ff55cbe1ca62a98e19
gwon522 <nesaz0522@naver.com>
2022-01-19 10:10:05 +0900

    [ADD] nodemon, express 추가

--- server/server.js
+++ server/server.js
@@ -2,3 +2,11 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
+const port = 5000;
+app.get('/',(req,res)=>{
+    res.send("hi");
+})
+
+app.listen(port,()=>{
+    console.log(`server on port ${port}`)
+})
