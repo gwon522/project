6fd7cafa9a565072688097474e62a2ef482faa1b
gwon <53310111+gwon522@users.noreply.github.com>
2022-01-20 23:58:03 +0900

    [ADD] 댓글 대댓글 추가 기능X

--- client/src/component/Comment.js
+++ client/src/component/Comment.js
@@ -1,20 +1,23 @@
 import { CommentText, CommentWrap, Name } from "styles/Comment.style"
-import { FuncSpan, NameColor, StyledLink, WrapInfo } from "styles/Global.style"
+import { FuncSpan, InfoFnc, NameColor, StyledLink, WrapInfo } from "styles/Global.style"
 
 
 export const Comment = (props) => {
     //대댓글 버튼, 댓글 클릭시 기능 => 로그인시 가능하게, 구현도 해야됨
     return (
-        <CommentWrap>
+        <CommentWrap reReply={props.reReply}>
             <Name>
                 <StyledLink style={NameColor} to="/">직장명</StyledLink> · 블라인드처리
             </Name>
             <CommentText>글내용</CommentText>
             <WrapInfo>
-                <FuncSpan>시간</FuncSpan>
-                <FuncSpan>좋아요</FuncSpan>
-                <FuncSpan>대댓글</FuncSpan>
+                <FuncSpan func="time">시간</FuncSpan>
+                <FuncSpan func="view">좋아요</FuncSpan>
+                <FuncSpan func="cmt">대댓글</FuncSpan>
+                <InfoFnc func="detail">
+                    <FuncSpan func="reply" />
+                </InfoFnc>
             </WrapInfo>
-        </CommentWrap>
+        </CommentWrap >
     )
 }
---
+++ client/src/component/ReComment.js
@@ -0,0 +1,50 @@
+import styled from "styled-components"
+import { Comment } from "./Comment"
+
+const ReCommentWrap = styled.div`
+    margin-top: -1px;
+    border-top: 1px solid #eee;
+    background-color: #f8f8f8;
+`
+const CommentButton = styled.button`
+    width: 100%;
+    margin: 24px 0 0;
+    border-color: #eee;
+    border-style: solid;
+    border-width: 1px 0;
+    background: #fff;
+    font-size: 14px;
+    line-height: 59px;
+    overflow: visible;
+    background-color: transparent;
+    cursor: pointer;
+
+    @media screen and (min-width:1100px){
+        width: 100%;
+        margin: 30px 0 0;
+        font-weight: 700;
+    }
+`
+const ReCommentButton = styled(CommentButton)`
+    margin-top: 0;
+    border-top: 0;
+    background-color: #f8f8f8;
+
+    @media screen and (min-width:1100px){
+        margin-left: 0;
+    }
+`
+
+export const ReComment = (props) => {
+    return (
+        <ReCommentWrap>
+            <ReCommentButton>
+                +대댓글 cnt개 더보기
+            </ReCommentButton>
+            {
+                //대댓글 수만큼 반복
+            }
+            <Comment />
+        </ReCommentWrap>
+    )
+}
